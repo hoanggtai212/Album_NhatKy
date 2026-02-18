4b14e2963fa89900da27097708d17476cff3d420
HT <rongchube99@gmail.com>
2026-02-19 01:54:34 +0700

    Update script.js

--- style/script.js
+++ style/script.js
@@ -32,7 +32,7 @@ book.appendChild(introPage);
 pages.push(introPage);
 
 const images = [];
-for (let i = 1; i <= 20; i++) { //Chỉnh số lượng ảnh ở đây
+for (let i = 1; i <= 18; i++) { //Chỉnh số lượng ảnh ở đây
   images.push(`./style/image/Anh (${i}).jpg`);
 }
 
@@ -157,5 +157,6 @@ pages.forEach((page) => {
   });
 
 });
+
 
 
